092695e41f913f5002b4086974c66d81ec50862e
Viswanathan <67451889+drummerviswa@users.noreply.github.com>
2022-09-19 09:43:07 +0530

    Add files via upload

---
+++ squaring.py
@@ -0,0 +1,5 @@
+print("Welcome to the squaring project")
+
+n = int(input("Enter the number you want to know the square value: "))
+nsq = n*n
+print("The square value of",n,"is",nsq)
---
+++ tables.py
@@ -0,0 +1,5 @@
+print("Welcome to the Multiplication table printing project")
+n = int(input("Enter the number you want to have the table: "))
+for i in range(1,10+1,1):
+    print(i,"X",n,"=",i*n)
+
